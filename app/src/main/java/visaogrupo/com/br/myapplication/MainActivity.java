1d298070e0bd228661de2c7c283d77a909dd4c76
lucas.honda <contatostts@gmail.com>
2022-07-20 18:16:27 -0300

    dsdsaasasdas

---
+++ app/src/main/java/visaogrupo/com/br/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package visaogrupo.com.br.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String ad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
